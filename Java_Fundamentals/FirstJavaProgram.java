78e1d4eb4dabe7699dd884d41dbef53a8903b230
mutaz jaber <mutaz-mhj@hotmail.com>
2023-12-26 11:54:32 +0200

    first commit

---
+++ Java_Fundamentals/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+
+public class FirstJavaProgram{
+    public static void main(String[] args){
+        System.out.println("My name is MUtaz jaber");
+        System.out.println("I am 30 years old");
+        System.out.println("My hometown is Burbank, CA");
+
+
+        
+}
+}
+
