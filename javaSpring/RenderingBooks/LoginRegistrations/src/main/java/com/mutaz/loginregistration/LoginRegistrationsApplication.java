c14bd0ce0660a6da4fde6f51c970f386cb63d295
mutaz jaber <mutaz-mhj@hotmail.com>
2024-01-16 20:29:42 +0200

    login

---
+++ javaSpring/RenderingBooks/LoginRegistrations/src/main/java/com/mutaz/loginregistration/LoginRegistrationsApplication.java
@@ -0,0 +1,13 @@
+package com.mutaz.loginregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginRegistrationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegistrationsApplication.class, args);
+	}
+
+}
---
+++ javaSpring/RenderingBooks/LoginRegistrations/src/test/java/com/mutaz/loginregistration/LoginRegistrationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mutaz.loginregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginRegistrationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
